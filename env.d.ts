ad79178410ccd24cfa674650839cd07bebbf45b2
492 <734204679@qq.com>
2023-11-01 19:23:02 +0800

    init

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+declare interface Window {
+  _AMapSecurityConfig: any
+  AMapUI: any
+  districtExplorer: any
+  currentAreaNode: any
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './assets/base.css'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import Vcharts from "vue-echarts"
+
+import App from './App.vue'
+import router from './router'
+
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.component('v-charts', Vcharts)
+
+app.mount('#app')
