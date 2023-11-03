ddbdfe2b0b5363b6ffec9b3d2890d6e8bfa3f87b
492 <734204679@qq.com>
2023-11-03 16:39:19 +0800

    更换echart

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createApp } from 'vue'
 
 import Vcharts from "vue-echarts"
 
+import { VueQueryPlugin } from "@tanstack/vue-query"
 import App from './App.vue'
 import router from './router'
 
@@ -14,6 +15,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 app.component('v-charts', Vcharts)
 
 app.mount('#app')
